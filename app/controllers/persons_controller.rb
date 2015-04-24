b7a6e39237682a49c4fc0ba9753d073e15f31d76
Albert Gareev <whosuchalbert@yandex.ru>
2015-04-25 00:32:40 +0300

    heroku 3.0

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,4 +1,19 @@
 class PersonsController < ApplicationController
+ before_action :signed_in_user,
+                only: [:index, :edit, :update, :destroy, :following, :followers]
+ def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   def profile
   	if user_signed_in? 
   		@posts  = current_user.posts
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
-module ApplicationHelper
+	module ApplicationHelper
+		 def current_user?(user)
+    user == current_user
+  end
 end
